ac0ed031eb72fea6ffcc111f070d01d860d29711
Thiago van Dieten <thiago@tvdieten.com>
2024-07-06 00:52:45 +0300

    start with conditionals

---
+++ ruby_basics/4_conditionals/conditionals.rb
@@ -0,0 +1,20 @@
+room_tidy = true
+
+attack_by_land = false
+attack_by_sea = true
+
+if room_tidy == true
+  puts "I can play video games"
+end
+
+if 1 < 2
+  puts "Wow, 1 do be lower than 2"
+end
+
+if attack_by_land
+  puts "release the goat"
+elsif attack_by_sea
+  puts "release the shark"
+else
+  puts "release Kevin the octopus"
+end
