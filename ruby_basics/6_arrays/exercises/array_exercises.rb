cbcad6536f3a600ca3039e2f2591d7e597b79e14
Thiago van Dieten <thiago@tvdieten.com>
2024-07-11 11:47:51 +0300

    apply rubocop suggestions
    Lots of unnecessary return statements. Just to fit in with the other
    Rubyists :)

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,34 +1,34 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
-  return Array.new(number)
+  Array.new(number)
 end
 
 def first_element(array)
   # return the first element of the array
-  return array.first()
+  array.first
 end
 
 def third_element(array)
   # return the third element of the array
-  return array[2]
+  array[2]
 end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
-  return array.last(3)
+  array.last(3)
 end
 
 def add_element(array)
   # add an element (of any value) to the array
-  array.push("I am here...now")
-  return array
+  array.push('I am here...now')
+  array
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
   array.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
-  return array
+  array
 end
 
 def remove_first_three_elements(array)
@@ -41,35 +41,35 @@ end
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
   original.concat(additional)
-  return original
+  original
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
-  return original.difference(comparison)
+  original.difference(comparison)
 end
 
 def empty_array?(array)
   # return true if the array is empty
-  return array.empty?
+  array.empty?
 end
 
 def reverse(array)
   # return the reverse of the array
-  return array.reverse
+  array.reverse
 end
 
 def array_length(array)
   # return the length of the array
-  return array.length
+  array.length
 end
 
 def include?(array, value)
   # return true if the array includes the value
-  return array.include?(value)
+  array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
-  return array.join(separator)
+  array.join(separator)
 end
